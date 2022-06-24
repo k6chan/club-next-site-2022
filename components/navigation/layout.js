a886512099e240888af4e48fb697c924f342e71f
k6chan <k6chan@ucsd.edu>
2022-06-23 23:56:23 -0500

    remove Animefest pages and component imports

--- components/navigation/layout.js
+++ components/navigation/layout.js
@@ -1,6 +1,5 @@
 import Link from 'next/link'
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
-import Sponsor from "./sponsor"
 
 
 export default function Layout({ children }) {
@@ -9,33 +8,16 @@ export default function Layout({ children }) {
       {/* Sticky top is not good for legacy, consider fixing the position instead (but that comes with covering up the page) */}
       <Navbar bg="light" expand="lg" sticky="top">
         <Container>
-          <Link href="/" passHref><Navbar.Brand className="display-text">ANIMEFEST</Navbar.Brand></Link>
+          <Link href="/" passHref><Navbar.Brand className="display-text">ANIME &amp; MANGA ENTHUSIASTS</Navbar.Brand></Link>
           <Navbar.Toggle aria-controls="basic-navbar-nav"/>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav navbarScroll className="ms-auto">
-              <NavDropdown title="Home" id="basic-nav-dropdown">
-                <Link href='/' passHref><NavDropdown.Item>Home</NavDropdown.Item></Link>
-                <NavDropdown.Divider />
-                <Link href='/#about' passHref><NavDropdown.Item>About</NavDropdown.Item></Link>
-                <Link href='/#map' passHref><NavDropdown.Item>Map and Schedule</NavDropdown.Item></Link>
-                <Link href='/#parking' passHref><NavDropdown.Item>Parking</NavDropdown.Item></Link>
+              <Link href='/' passHref><Nav.Link>EVENTS</Nav.Link></Link>
+              <NavDropdown title="ABOUT" id="basic-nav-dropdown">
+                <Link href='/' passHref><NavDropdown.Item>HISTORY</NavDropdown.Item></Link>
+                <Link href='/' passHref><NavDropdown.Item>OFFICERS</NavDropdown.Item></Link>
               </NavDropdown>
-              <NavDropdown title="Programming" id="basic-nav-dropdown">
-                <NavDropdown.Header>West Ballroom</NavDropdown.Header>
-                <Link href="/mainstage" passHref><NavDropdown.Item>Main Stage</NavDropdown.Item></Link>
-                <Link href="/raffles" passHref><NavDropdown.Item>Raffles</NavDropdown.Item></Link>
-                <Link href="/exhibitor" passHref><NavDropdown.Item>Exhibitor Hall</NavDropdown.Item></Link>
-                <NavDropdown.Header>Price Center Theater</NavDropdown.Header>
-                <Link href="/theater" passHref><NavDropdown.Item>Theater</NavDropdown.Item></Link>
-                <NavDropdown.Header>College Rooms</NavDropdown.Header>
-                <Link href="/panels" passHref><NavDropdown.Item>Panels</NavDropdown.Item></Link>
-                <Link href="/manga" passHref><NavDropdown.Item>Manga Library</NavDropdown.Item></Link>
-                <NavDropdown.Header>Sun God Lounge</NavDropdown.Header>
-                <Link href="/gaming" passHref><NavDropdown.Item>Gaming</NavDropdown.Item></Link>
-                <Link href="/activities" passHref><NavDropdown.Item>Featured Activities</NavDropdown.Item></Link>
-              </NavDropdown>
-              <Link href='/food' passHref><Nav.Link>Food</Nav.Link></Link>
-              <Link href='/rules' passHref><Nav.Link>Rules</Nav.Link></Link>
+              <Link href='/' passHref><Nav.Link>JOIN US</Nav.Link></Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
@@ -43,9 +25,7 @@ export default function Layout({ children }) {
       <main>{children}</main>
       <footer className="footer">
         <Container className="text-center">
-          <h5 className="mb-3">Sponsored By</h5>
-          <Sponsor />
-          <p className="caption-text">&copy; 2022 Cal Animage Beta. Website design by Kaitlyn Chan. All assets belong to their respective owners.</p>
+          <p className="caption-text">&copy; 2022 Anime &amp; Manga Enthusiasts at UC San Diego. Website design by Kaitlyn Chan. All assets belong to their respective owners.</p>
         </Container>
       </footer>
     </>
--- pages/index.js
+++ pages/index.js
@@ -1,80 +1,10 @@
-import { Col, Container, Ratio, Row } from 'react-bootstrap'
-import BackgroundImage from "../components/index/backgroundimage"
-import ProgramsCarousel from "../components/programscarousel"
-import Social from "../components/index/social"
-import MyImage from "../components/myimage"
-import Programs from "../data/programs.json"
+import { Container } from 'react-bootstrap'
+// import MyImage from "../components/myimage"
 
 
 export default function Home() {
     return (
         <>
-            <Container fluid className="text-center my-1 text-danger">
-                UCSD students and staff must show a green thumb to attend the convention. All other attendees must show
-                a proof of vaccination or
-                a negative COVID-19 test result received within the last 48 hours.
-            </Container>
-
-            <Container fluid className="text-center vh-100 p-0">
-                <BackgroundImage src="images/landing.jpg" className="landing">
-                    <Row className="align-items-center h-75 g-0 glow-text">
-                        <Col>
-                            <MyImage src="images/logo_af.svg" className="w-75" alt="Animefest 2022 logo" />
-                            <h4>May 7 at UC San Diego&apos;s Price Center</h4>
-                            <h5>11:00 AM to 8:00 PM</h5>
-                            <h5>Free Admission</h5>
-                        </Col>
-                    </Row>
-                </BackgroundImage>
-            </Container>
-
-            <Social>
-                <p className="mb-0 caption-text">Contact us and get live event coverage on our social media!</p>
-            </Social>
-
-            <Container fluid className="p-5 section" id="about">
-                <h1 className="text-left mb-4 display-text">ABOUT</h1>
-                <p>Animefest, a time and place where animanga lovers, artists, and entertainers celebrate what we love!
-                    On May 7, 2022, participants go to the Price Center at UCSD to attend discussion and workshop
-                    panels, buy the best artwork,
-                    and check out your favorite manga in our manga library! And of course, there will be cosplays,
-                    festival games, and video games&mdash;all for some otaku fun.</p>
-                <p className="mb-4">For a taste of the convention, take a look at our recap video of Animefest 2019:</p>
-                <div className="video-player mx-auto mb-5">
-                    <Ratio aspectRatio="16x9">
-                        <iframe className="border-stroke"
-                            src="https://www.youtube-nocookie.com/embed/eITsJPrRF4c"
-                            title="YouTube video player" frameBorder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                            allowFullScreen={true} />
-                    </Ratio>
-                </div>
-                <ProgramsCarousel data={Programs["programs"]} />
-            </Container>
-
-            <Container fluid className="p-5 mt-4 section" id="map">
-                <h1 className="text-left mb-4 display-text">MAP AND SCHEDULE</h1>
-                <p>Animefest is held at the Price Center at UC San Diego. Specifically, it will be at the West Ballroom, Thurgood Marshall,
-                    Eleanor Roosevelt, Roger Revelle, Theater, Red Shoe, Bear, and Green Table
-                    rooms.</p>
-                <p className="mb-4">All rooms are located on the second floor with the exception of the Theater, which is on the first
-                    floor and below the Bear Room.</p>
-                <div className="text-center mb-5">
-                    <iframe className="sheet-schedule sheet-constraint w-75 d-block mb-1 mx-auto"
-                        src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSh2gBYXSEV57p05RAVENMwqDHmJ5GFHTjgb0M9LOTAYrBbLrrwA5fMECz7R3F2qfpnRElr854rGZOG/pubhtml?widget=true&amp;headers=false" />
-                    <p className="caption-text"><a
-                        href="https://docs.google.com/spreadsheets/d/11egWL19jhBBIf27SXAjf81tyDxkVJsRjCtNo4gr5bTE/edit?usp=sharing">Have
-                        trouble viewing? Click here!</a></p>
-                </div>
-                <div className="text-center">
-                    <a href="images/map.jpg">
-                        <MyImage src="images/map.jpg" className="sheet-constraint border-stroke mw-100 mb-1"
-                            alt="Price Center map"></MyImage>
-                    </a>
-                    <p className="caption-text mb-0">Click on the image to open.</p>
-                </div>
-            </Container>
-
             <Container fluid className="p-5 mt-4 section" id="parking">
                 <h1 className="text-left mb-4 display-text">PARKING</h1>
                 <p>Unfortunately, <a href="https://transportation.ucsd.edu/visit/visitor/index.html">parking is not
@@ -89,13 +19,6 @@ export default function Home() {
                 <p className="mb-4">
                     The Gilman Parking Structure is the closest parking structure to the convention. You can use the
                     Wayfinding feature or the map below to navigate to Price Center:</p>
-                <div className="map mx-auto">
-                    <Ratio aspectRatio="4x3">
-                        <iframe className="border-stroke"
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3350.697394192468!2d-117.2383838856308!3d32.87972498605018!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dc06c46a524fb9%3A0x68571efb243bc289!2sPrice%20Center%2C%20La%20Jolla%2C%20CA%2092093!5e0!3m2!1sen!2sus!4v1638064542514!5m2!1sen!2sus"
-                            width="600" height="450" allowFullScreen="" loading="lazy" />
-                    </Ratio>
-                </div>
             </Container>
         </>
     )
