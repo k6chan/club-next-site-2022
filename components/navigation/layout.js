c5497f5a1d31b7a6168fb5434d29e15333cf7fff
k6chan <k6chan@ucsd.edu>
2022-06-24 00:07:38 -0500

    attribution

--- components/navigation/layout.js
+++ components/navigation/layout.js
@@ -24,7 +24,7 @@ export default function Layout({ children }) {
       <main>{children}</main>
       <footer className="footer">
         <Container className="text-center">
-          <p className="caption-text">&copy; 2022 Anime &amp; Manga Enthusiasts at UC San Diego. Website design by Kaitlyn Chan.</p>
+          <p className="caption-text">&copy; 2022 Anime &amp; Manga Enthusiasts at UC San Diego. Website design by Kaitlyn Chan. All assets belong to their respective owners.</p>
         </Container>
       </footer>
     </>
