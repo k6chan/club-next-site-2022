69f325d52a51a9e88ee4957e7b973bbd6319a207
k6chan <k6chan@ucsd.edu>
2022-06-24 22:57:55 -0500

    use uppercase text class

--- components/navigation/layout.js
+++ components/navigation/layout.js
@@ -6,17 +6,17 @@ export default function Layout({ children }) {
   return (
     <>
       <Navbar bg="light" expand="lg" sticky="top">
-        <Container className="title-font">
-          <Link href="/" passHref><Navbar.Brand>ANIME &amp; MANGA ENTHUSIASTS</Navbar.Brand></Link>
+        <Container className="title-font text-uppercase">
+          <Link href="/" passHref><Navbar.Brand>Anime &amp; Manga Enthusiasts</Navbar.Brand></Link>
           <Navbar.Toggle aria-controls="basic-navbar-nav"/>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav navbarScroll className="ms-auto">
-              <Link href='/' passHref><Nav.Link>EVENTS</Nav.Link></Link>
+              <Link href='/' passHref><Nav.Link>Events</Nav.Link></Link>
               <NavDropdown title="ABOUT" id="basic-nav-dropdown">
-                <Link href='/' passHref><NavDropdown.Item>HISTORY</NavDropdown.Item></Link>
-                <Link href='/' passHref><NavDropdown.Item>OFFICERS</NavDropdown.Item></Link>
+                <Link href='/' passHref><NavDropdown.Item>History</NavDropdown.Item></Link>
+                <Link href='/' passHref><NavDropdown.Item>Officers</NavDropdown.Item></Link>
               </NavDropdown>
-              <Link href='/' passHref><Nav.Link>JOIN US</Nav.Link></Link>
+              <Link href='/' passHref><Nav.Link>Join Us</Nav.Link></Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
