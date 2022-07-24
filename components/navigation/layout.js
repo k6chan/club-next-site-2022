c0caac4eaccd3ba5fdf910b64ef25809801efa62
k6chan <k6chan@ucsd.edu>
2022-07-23 23:25:48 -0500

    add logo to navbar

--- components/navigation/layout.js
+++ components/navigation/layout.js
@@ -1,24 +1,27 @@
 import Link from 'next/link'
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import { Container, Nav, Navbar, NavDropdown, Row, Col } from 'react-bootstrap'
+import MyImage from "../myimage"
 
 
 export default function Layout({ children }) {
   return (
     <>
-      <Navbar bg="light" expand="lg" sticky="top">
+      <Navbar bg="dark" expand="lg" sticky="top" variant="dark">
         <Container className="title-font text-uppercase">
-          <Link href="/" passHref><Navbar.Brand>Anime &amp; Manga Enthusiasts</Navbar.Brand></Link>
-          <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav navbarScroll className="ms-auto">
-              <Link href='/' passHref><Nav.Link>Events</Nav.Link></Link>
-              <NavDropdown title="ABOUT" id="basic-nav-dropdown">
-                <Link href='/' passHref><NavDropdown.Item>History</NavDropdown.Item></Link>
-                <Link href='/' passHref><NavDropdown.Item>Officers</NavDropdown.Item></Link>
-              </NavDropdown>
-              <Link href='/' passHref><Nav.Link>Join Us</Nav.Link></Link>
-            </Nav>
-          </Navbar.Collapse>
+            <Navbar.Brand href="/">
+                <MyImage alt="AME logo" src="../images/AME_navbar_logo.svg" className="d-inline-block w-50"/>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav navbarScroll className="ms-auto">
+                    <Link href='/' passHref><Nav.Link>Events</Nav.Link></Link>
+                    <NavDropdown title="ABOUT" id="basic-nav-dropdown">
+                        <Link href='/' passHref><NavDropdown.Item>History</NavDropdown.Item></Link>
+                        <Link href='/' passHref><NavDropdown.Item>Officers</NavDropdown.Item></Link>
+                    </NavDropdown>
+                    <Link href='/' passHref><Nav.Link>Join Us</Nav.Link></Link>
+                </Nav>
+            </Navbar.Collapse>
         </Container>
       </Navbar>
       <main>{children}</main>
