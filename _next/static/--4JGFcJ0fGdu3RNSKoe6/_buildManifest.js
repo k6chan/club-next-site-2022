7a1d9536d10435e9bdd1f0faf5ce92f549bde087
k6chan <k6chan@users.noreply.github.com>
2022-07-26 06:15:54 +0000

    deploy: 039c5948e8af491543c97fc02e65504a426e99fa

---
+++ _next/static/--4JGFcJ0fGdu3RNSKoe6/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-a1ad981ca1f5c0ee.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],"/sandbox":["static/chunks/pages/sandbox-251fd7957e355205.js"],sortedPages:["/","/_app","/_error","/sandbox"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
