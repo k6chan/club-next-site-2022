58b1eb09d51419b52d3072c3cd1d94a17f4447c6
k6chan <k6chan@ucsd.edu>
2022-07-25 21:15:58 -0500

    add link support in carousel

--- components/bannercarousel.js
+++ components/bannercarousel.js
@@ -2,6 +2,18 @@ import { Carousel} from "react-bootstrap"
 import MyImage from "./myimage";
 
 
+export function CaptionLink(data) {
+  // takes a carousel item with optional "link" property
+  if (data["data"]["link"]) {
+    return (
+        <p><a href={data["data"]["link"]["address"]}>{data["data"]["link"]["name"]}</a></p>
+    )
+  }
+  else {
+    return <></>
+  }
+}
+
 export default function BannerCarousel({ data, ...otherProps }) {
   return (
     <Carousel {...otherProps}>
@@ -11,6 +23,7 @@ export default function BannerCarousel({ data, ...otherProps }) {
             <Carousel.Caption className="title-font text-uppercase">
               <h2>{banner["title"]}</h2>
               <p>{banner["description"]}</p>
+              <CaptionLink data={banner}/>
             </Carousel.Caption>
           </Carousel.Item>
       ))}
