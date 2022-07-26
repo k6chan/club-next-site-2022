0fa04b70f9cebdb162169298a5a07bfda0a51804
k6chan <k6chan@ucsd.edu>
2022-07-26 00:08:10 -0500

    add final section

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import BannerCarousel from "../components/bannercarousel"
+import MyImage from "../components/myimage"
 import Banners from "../data/banners.json"
-import {Row, Col, Container, Ratio, Button} from "react-bootstrap";
+import {Row, Col, Container, Ratio, Button} from "react-bootstrap"
 
 
 export default function Home() {
@@ -22,7 +23,7 @@ export default function Home() {
                     <Col md={7}>
                         <Ratio aspectRatio="4x3">
                             <iframe
-                                src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FLos_Angeles&src=dWNzZGFuaW1lY2x1YkBnbWFpbC5jb20&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%237986CB&color=%237986CB"
+                                src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FLos_Angeles&src=dWNzZGFuaW1lY2x1YkBnbWFpbC5jb20&color=%23039BE5"
                                 width="800" height="600">
                             </iframe>
                         </Ratio>
@@ -31,20 +32,37 @@ export default function Home() {
             </Container>
             <Container className="border-bottom mb-5">
                 <Row className="align-items-center mb-5">
-                    <Col md={7}>
+                    <Col md={6}>
                         <iframe src="https://discord.com/widget?id=247566229383020546&theme=dark" width="350"
-                                height="500" allowTransparency="true" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+                                height="500" allowTransparency="true"
+                                sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
                     </Col>
-                    <Col md={5}>
+                    <Col md={6}>
                         <p className="title-size title-font text-uppercase border-bottom">Who are we?</p>
-                        <p>We&apos;re the anime club at UCSD :D We might seem mysterious at first, but stop by club sometime to
-                            really get to know us. Don&apos;t be intimidated if you can&apos;t show up to the first couple of
+                        <p>We&apos;re the anime club at UCSD :D We might seem mysterious at first, but stop by club
+                            sometime to
+                            really get to know us. Don&apos;t be intimidated if you can&apos;t show up to the first
+                            couple of
                             meetings or events. Feel free to hang out with us at any of the things that we host
                             throughout the school year anytime.</p>
                         <div className="d-grid gap-2">
                             <Button variant="secondary" size="lg">History</Button>
                             <Button variant="secondary" size="lg">Current Officers</Button>
                         </div>
+                    </Col>
+                </Row>
+            </Container>
+            <Container className="border-bottom">
+                <Row className="align-items-center mb-5">
+                    <Col md={7}>
+                        <p className="title-size title-font text-uppercase border-bottom">What Do We Do?</p>
+                        <p>We host anime showings and other activities every Monday evening. We also hang out and
+                            organize weekend socials like karaoke, club dinner, etc. Our largest event is our on-campus
+                            con, Animefest, which takes place every Winter Quarter.</p>
+                    </Col>
+                    <Col md={5}>
+                        <MyImage src="images/mascot_images/triton_sitting.png" alt="Triton sitting"
+                                 className="w-100"></MyImage>
                     </Col>
                 </Row>
             </Container>
