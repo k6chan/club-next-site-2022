4c6a8e13f240627ab341ecc7005d3afeac8a1943
k6chan <k6chan@ucsd.edu>
2022-07-26 22:54:57 -0500

    move font declaration

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,24 @@
-import { useRouter } from "next/dist/client/router"
+import {useRouter} from "next/dist/client/router"
 import Head from 'next/head'
 import Layout from '../components/navigation/layout'
 import '../styles/globals.css'
 import '../styles/theme.scss'
 
 
-export default function MyApp({ Component, pageProps }) {
-  const router = useRouter()
-  return (
-    <>
-      <Head>
-        <title>Anime &amp; Manga Enthusiasts</title>
-        <meta name="description" content="Anime Club at UC San Diego" />
-        <link rel="icon" href={`${router.basePath}/favicon.ico`}/>
-      </Head>
+export default function MyApp({Component, pageProps}) {
+    const router = useRouter()
+    return (
+        <>
+            <Head>
+                <title>Anime &amp; Manga Enthusiasts</title>
+                <meta name="description" content="Anime Club at UC San Diego"/>
+                <link rel="icon" href={`${router.basePath}/favicon.ico`}/>
+                <link rel="stylesheet" href={`${router.basePath}/fonts.css`}/>
+            </Head>
 
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </>
-  )
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </>
+    )
 }
