122df4a1915bfc54198ac0b6b7a8ac671d6cb235
k6chan <k6chan@ucsd.edu>
2022-07-28 00:07:06 -0500

    change banner image

--- pages/history.js
+++ pages/history.js
@@ -7,7 +7,7 @@ export default function History() {
     return (
         <>
             <Container fluid className="text-center p-0 mb-4" style={{"height": "25vh"}}>
-                <BackgroundImage src="images/mascots_banner.png" className="landing">
+                <BackgroundImage src="images/geisel_banner.jpg" className="landing">
                     <Row className="align-items-center h-100 g-0">
                         <Col>
                             <h1 className="text-uppercase title-font-condensed text-light display-size glow-text"><span
