0a6c92bb96a7ef1f3fe8debf01a24fc3c7eff760
k6chan <k6chan@ucsd.edu>
2022-07-29 02:48:16 -0500

    change gradient and font

--- components/bannercarousel.js
+++ components/bannercarousel.js
@@ -8,7 +8,7 @@ function CarouselBackground({image}) {
         backgroundPosition: `center`,
         backgroundSize: `cover`,
         height: `500px`,
-        backgroundImage: `linear-gradient(to top, rgb(0, 0, 0) 0%, rgba(255, 255, 255, 0) 40%), url('${router.basePath}/${image}')`
+        backgroundImage: `linear-gradient(to top, rgb(33, 37, 41) 0%, rgba(255, 255, 255, 0) 40%), url('${router.basePath}/${image}')`
     }
     return (
         <div className="w-100" style={myBackgroundStyle}></div>
@@ -21,8 +21,8 @@ export default function BannerCarousel({banners, ...otherProps}) {
             {banners.map((banner, index) => (
                 <Carousel.Item key={index}>
                     <CarouselBackground image={banner["image"]}/>
-                    <Carousel.Caption className="title-font text-uppercase glow-text">
-                        <h2>{banner["title"]}</h2>
+                    <Carousel.Caption className="glow-text">
+                        <h2 className="title-font text-uppercase">{banner["title"]}</h2>
                         <p>{banner["description"]}</p>
                         {banner["link"] && <p><a href={banner["link"]["address"]}>{banner["link"]["name"]}</a></p>}
                     </Carousel.Caption>
