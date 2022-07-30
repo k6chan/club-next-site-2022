19255305e4f30887880ebe38d442e9e54844d7a4
k6chan <k6chan@ucsd.edu>
2022-07-30 00:42:36 -0500

    load in officer information

--- pages/officers.js
+++ pages/officers.js
@@ -1,14 +1,56 @@
-import {Container} from "react-bootstrap"
+import {Col, Container, Row} from "react-bootstrap"
 import PageHeader from "../components/pageheader"
+import MyImage from "../components/myimage"
+import OfficerData from "../data/officers.json"
 
+
+function ContactDisplay({contact}) {
+    if (contact["url"]) {
+        return (
+            <p className="mb-0">{contact["title"]}: <a href={contact["url"]}>{contact["handle"]}</a></p>
+        )
+    } else {
+        return (
+            <p className="mb-0">{contact["title"]}: {contact["handle"]}</p>
+        )
+    }
+}
+
+function OfficerDisplay({officer}) {
+    return (
+        <Row className="align-items-top" id={officer["name"]}>
+            <Col xs={12} sm={12} md={7} className="py-2">
+                <p className="title-size title-font-condensed text-uppercase border-bottom">{officer["name"]}</p>
+                {officer["description"].map((paragraph, index) => (
+                    <p key={index}>{paragraph}</p>
+                ))}
+                <br/>
+                <p className="mb-0">Contact:</p>
+                {officer["contact"].map((contact, index) => (
+                    <ContactDisplay contact={contact} key={index}/>
+                ))}
+            </Col>
+            <Col xs={12} sm={12} md={5} className="py-2">
+                <MyImage src={officer["image"]} alt={officer["name"]}
+                         className="w-100 rounded-circle"/>
+            </Col>
+        </Row>
+    )
+}
 
 export default function Officers() {
+    // sort officers by name
+    const officers_sorted = OfficerData["officers"].sort((o1, o2) => o1["name"].localeCompare(o2["name"]))
     return (
-        <div className="min-vh-100">
-            <PageHeader src="images/header_images/officers_banner.png" className="mb-4">Officers</PageHeader>
-            <Container>
-                <p>Placeholder</p>
-            </Container>
-        </div>
+        <>
+        <PageHeader src="images/header_images/officers_banner.png" className="mb-4">Officers</PageHeader>
+        <Container className="mb-4">
+            <div className="d-grid gap-5">
+            {officers_sorted.map((officer, index) => (
+                <OfficerDisplay officer={officer} key={index}/>
+            ))}
+            </div>
+        </Container>
+        </>
     )
 }
