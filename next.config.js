5eab9b344153f499d43fcb92a2520e6e2af050b6
k6chan <77877660+k6chan@users.noreply.github.com>
2022-12-20 02:46:50 -0600

    update sub directory name

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ const useSubDirectory = process.env.USE_SUB_DIRECTORY === 'true'
 
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: useSubDirectory ? '/club-next-site/' : '',
-  basePath: useSubDirectory ? '/club-next-site' : ''
+  assetPrefix: useSubDirectory ? '/club-next-site-2022/' : '',
+  basePath: useSubDirectory ? '/club-next-site-2022' : ''
 }
